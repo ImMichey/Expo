d51c389c36af46ce4ed5556d42a044282ff7460f
Michey <michaelnickel@outlook.com>
2024-07-02 16:44:03 +0200

    Fixed debug VRAM data dump

--- server/src/main/java/dev/michey/expo/server/util/ExpoHardware.java
+++ server/src/main/java/dev/michey/expo/server/util/ExpoHardware.java
@@ -4,6 +4,9 @@ import dev.michey.expo.log.ExpoLogger;
 import oshi.SystemInfo;
 import oshi.hardware.GraphicsCard;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
 public class ExpoHardware {
 
     public static SystemInfo si = new SystemInfo();
@@ -25,7 +28,7 @@ public class ExpoHardware {
                 builder.append(" [");
 
                 GraphicsCard gpu = list.get(i);
-                builder.append(gpu.getName()).append(", deviceId=").append(gpu.getDeviceId()).append(", vendor=").append(gpu.getVendor()).append(", vRam=").append(gpu.getVendor()).append(", versionInfo=").append(gpu.getVersionInfo());
+                builder.append(gpu.getName()).append(", deviceId=").append(gpu.getDeviceId()).append(", vendor=").append(gpu.getVendor()).append(", vRam=").append(humanReadableByteCountBin(gpu.getVRam())).append(", versionInfo=").append(gpu.getVersionInfo());
                 builder.append("]");
 
                 if(i < (list.size() - 1)) {
@@ -38,6 +41,25 @@ public class ExpoHardware {
         ExpoLogger.log(gpuString);
 
         ExpoLogger.log("Memory: " + si.getHardware().getMemory().toString());
+    }
+
+    public static String humanReadableByteCountBin(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+
+        if(absB < 1024) {
+            return bytes + " B";
+        }
+
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+
+        for(int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+
+        value *= Long.signum(bytes);
+        return String.format("%.1f %ciB", value / 1024.0, ci.current());
     }
 
 }
